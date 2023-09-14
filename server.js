5c33699793777fa3223c86074bbfd6b8c3dd2a13
joseph <jomill0723@outlook.com>
2023-09-14 18:13:27 -0400

    added notes to code.

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
+// Import required library
 const express = require('express');
 const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require('path');
+
+// Import of array of notes
 const notes = require('./db/db.json');
 
+// Middlware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// Define route to get notes
 app.get('/api/notes', (req, res) => {
     res.json(notes.slice(1));
 });
 
+// Define route for homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// Define catch all route
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// Function to create new note and updates file
 function createNote(body, notesArray) {
     const newNote = body;
     if (!Array.isArray(notesArray)) {
@@ -40,11 +48,13 @@ function createNote(body, notesArray) {
     return newNote;
 }
 
+// Define a route for POST requests after creating notes
 app.post('/api/notes', (req, res) => {
     const newNote = createNote(req.body, notes);
     res.json(newNote);
 });
 
+// Function to delete note by ID
 function deleteNote(id, notesArray) {
     for (let i = 0; i < notesArray.length; i++) {
         let note = notesArray[i];
@@ -59,11 +69,13 @@ function deleteNote(id, notesArray) {
     }
 }
 
+// Define route to handle delete requests
 app.delete('/api/notes/:id', (req, res) => {
     deleteNote(req.params.id, notes);
     res.json(true);
 });
 
+// Starting server and listening for port
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
